7db2c9e473becded1b5a93e2052c84cf4cdac8e2
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-26 03:09:26 +0300

    feat: implement controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,33 @@
+import { IMail } from '../models/mail';
+import UserModel from '../schemas/userSchema';
+
+const getUser = async (params: string) => {
+  const foundedUser = await UserModel.findOne({ name: params });
+  if (foundedUser) {
+    return foundedUser;
+  }
+  const newUser = new UserModel({ name: params, mails: [] });
+  await newUser.save();
+  return newUser;
+};
+
+const getUsersName = async () => {
+  const foundedUsers = await UserModel.find({}, { name: 1 });
+  return foundedUsers;
+};
+
+const getUsersByNamePart = async (partName: string) => {
+  const foundedUsers = await UserModel.find({ name: { $regex: `${partName}` } }, { name: 1 });
+  return foundedUsers;
+};
+
+const updateUsers = async (userNameFrom: string, userNameTo: string, mail: IMail) => {
+  await UserModel.updateMany(
+    {
+      $and: [{ name: userNameFrom }, { name: userNameTo }],
+    },
+    { $push: { mails: mail } },
+  );
+};
+
+export { getUser, getUsersName, getUsersByNamePart, updateUsers };
